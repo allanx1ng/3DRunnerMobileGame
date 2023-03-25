5e8f86f7ee637cb4ffd94b62811f3a79b24d4b57
Eric <xericf@gmail.com>
2023-03-24 21:06:03 -0600

    Done env generator, imported env obj

--- Assets/Scripts/Terrains/EnvironmentGenerator.cs
+++ Assets/Scripts/Terrains/EnvironmentGenerator.cs
@@ -5,20 +5,21 @@ using UnityEngine;
 public class EnvironmentGenerator : MonoBehaviour
 {
 
-    private float environmentMargin = 15f;
-    [SerializeField] private float leftBoundary = -13f;
-    [SerializeField] private float rightBoundary = 13f;
+    // How far away an environment object MUST at least be from another + its bounding box extents
+    private float environmentMargin = 1f;
+    [SerializeField] private float leftBoundary = -13.5f;
+    [SerializeField] private float rightBoundary = 13.5f;
 
     [SerializeField] private List<GameObject> environmentObjects = new List<GameObject>();
 
     [SerializeField] private List<GameObject> walls = new List<GameObject>();
 
-
-    private int spawnAttempts = 5;
+    private int spawnAttempts = 9;
 
     private Renderer objectRenderer;
     private Bounds objectBounds;
     
+    private const float SQRT_2 = 1.41422f;
 
     // Start is called before the first frame update
     void Start()
@@ -48,8 +49,8 @@ public class EnvironmentGenerator : MonoBehaviour
         right.x += rightBoundary;
         right.y = 1f;
 
-        Quaternion leftRotation = Quaternion.Euler(-90, Random.Range(0, 4) * 90, 0); 
-        Quaternion rightRotation = Quaternion.Euler(-90, Random.Range(0, 4) * 90, 0); 
+        Quaternion leftRotation = Quaternion.Euler(0, Random.Range(0, 4) * 90, 0); 
+        Quaternion rightRotation = Quaternion.Euler(0, Random.Range(0, 4) * 90, 0); 
         GameObject leftWall = Instantiate(walls[leftWallModel], left, leftRotation);
         GameObject rightWall = Instantiate(walls[leftWallModel], right, rightRotation);
 
@@ -86,12 +87,12 @@ public class EnvironmentGenerator : MonoBehaviour
             Vector3 candidatePosition = new Vector3(Random.Range(minX, maxX), currentPosition.y + 0.5f, currentPosition.z);
             
             GameObject environmentObjectPrefab = ChooseEnvironmentObject();
-            // Bounds environmentBounds = BoundsHelper.GetBounds(environmentObjectPrefab);
-            // float minDistance = Mathf.Max(environmentBounds.extents.x, environmentBounds.extents.z) + environmentMargin; // arbitrary choose the larger width vs length (don't look at height)
-            float minDistance = environmentMargin;
-            if (CanSpawn(candidatePosition, minDistance, prevTerrain)) {
-                Quaternion rotation = Quaternion.Euler(-90, 0, 0); 
-                GameObject spawnedEnvironmentObject = Instantiate(environmentObjectPrefab, candidatePosition, rotation);
+
+            Bounds environmentBounds = BoundsHelper.GetBounds(environmentObjectPrefab);
+            float minDistanceFromObject = Mathf.Max(environmentBounds.extents.x * SQRT_2, environmentBounds.extents.z * SQRT_2) + environmentMargin; // arbitrary choose the larger width vs length (don't look at height)
+            // float minDistanceFromObject = environmentMargin;
+            if (CanSpawn(candidatePosition, minDistanceFromObject, prevTerrain)) {
+                GameObject spawnedEnvironmentObject = Instantiate(environmentObjectPrefab, candidatePosition, Quaternion.identity);
                 spawnedEnvironmentObject.transform.parent = transform;
             }
 
@@ -103,20 +104,29 @@ public class EnvironmentGenerator : MonoBehaviour
         int idx = Random.Range(0, environmentObjects.Count);
         return environmentObjects[idx];
     }
-    private bool CanSpawn(Vector3 candidatePosition, float minDistance, GameObject prevTerrain) {
+    private bool CanSpawn(Vector3 candidatePosition, float minDistanceFromObject, GameObject prevTerrain) {
         if (prevTerrain == null) return true; // can always spawn if there was no previous terrain
-        
+
         // loop through each of the previous terrain's children and check if they are spaced enough apart
         foreach (Transform childTransform in prevTerrain.transform) {
             if (childTransform.gameObject.tag != "Default Environment") continue;
-            if (Vector3.Distance(childTransform.position, candidatePosition) < minDistance) return false;
-        }
+            Bounds bounds = BoundsHelper.GetBounds(childTransform.gameObject);
+            float biggerDimension = Mathf.Max(bounds.extents.x, bounds.extents.z);
+            float minDistanceOverall = minDistanceFromObject + (biggerDimension * SQRT_2);
+            Debug.Log(minDistanceOverall);
+            if (Vector3.Distance(childTransform.position, candidatePosition) < minDistanceOverall) return false;
+        } 
 
         // loop through all of the current terrain's children (what env objects are already spawned)
         foreach (Transform childTransform in transform) {
             if (childTransform.gameObject.tag != "Default Environment") continue;
-            if (Vector3.Distance(childTransform.position, candidatePosition) < minDistance) return false;
+            Bounds bounds = BoundsHelper.GetBounds(childTransform.gameObject);
+            float biggerDimension = Mathf.Max(bounds.extents.x, bounds.extents.z);
+            float minDistanceOverall = minDistanceFromObject + (biggerDimension * SQRT_2);
+            if (Vector3.Distance(childTransform.position, candidatePosition) < minDistanceOverall) return false;
         }
+
+        
 
         return true;
     }

76c680b385d59567f9421ca4b13df3925613824f
Eric <xericf@gmail.com>
2023-05-20 15:33:28 -0600

    Made clustered obstacle gen

--- Assets/Scripts/Obstacles/ObstacleGenerator.cs
+++ Assets/Scripts/Obstacles/ObstacleGenerator.cs
@@ -4,9 +4,23 @@ using UnityEngine;
 
 public class ObstacleGenerator : MonoBehaviour
 {
+    [TextArea]
+    public string SpawningComment = "This handles the spawning behaviour of the obstacles, the minimum and maximum distance between clusters, and size of clusters";
+
+    public const int MIN_SIZE_MARGIN = 8;
+    public const int MAX_SIZE_MARGIN = 18;
+    public const int MIN_SIZE_CLUSTER = 3;
+    public const int MAX_SIZE_CLUSTER = 12;
+
+    private int currentSizeCluster = MIN_SIZE_CLUSTER;
+    private int currentSizeMargin = MIN_SIZE_MARGIN;
+    private int clusterIndex = 0; // the index on the current cluster
+    private int marginIndex = 0; // the index when not spawning a cluster, 
     
-    // The size of the block should include the MARGIN that a block should have between other blocks
-    public float sizeOfBlock = 5f;
+    
+    [TextArea]
+    public string ChanceComment = "Inbetween clusters, there will be areas of empty space in the margins. Here it can spawn things based on these probabilities";
+    public float sizeOfBlock = 5f; // The size of the block should include the MARGIN that a block should have between other blocks
     public float chanceForNothing = 85f;
     public float chanceForMob = 10f;
     public float chanceForBlock = 5f;
@@ -35,35 +49,63 @@ public class ObstacleGenerator : MonoBehaviour
         obstacleRow.transform.SetParent(obstacleHolder.transform);
         obstacleRows.Add(obstacleRow);
 
-        // Chance of spawning nothing vs. block vs. mob
-        float totalChance = chanceForBlock + chanceForMob + chanceForNothing;
-
         // Dont generate obstacles for objects with no children (roads)
         if (terrainData.canSpawnBlocks) {
-            for (int i = 0; i < amount; i++) {
+            if (marginIndex < currentSizeMargin) {
+                // spawns an object (or not) based on probabilities for the void space
+                spawnRowMargin(startX, amount, terrainData, position, obstacleRow);
+                marginIndex++;
+            } else if (clusterIndex < currentSizeCluster) {
+                spawnRowCluster(startX, amount, terrainData, position, obstacleRow);
+                clusterIndex++;
+            } else {
+                // reset the indices
+                clusterIndex = 0;
+                marginIndex = 0;
 
-                float randomNumber = Random.Range(0f, totalChance);
-                float newX = startX + (i * sizeOfBlock);
-
-                if (randomNumber <= chanceForNothing) {
-                    // do nothing
-                    continue;
-                } else if (randomNumber <= chanceForMob + chanceForNothing) {
-                    // spawn mob
-                    GameObject mob = ChooseMob(terrainData);
-                    mob.tag = "Mob";
-                    GenerateObstacle(new Vector3(newX, position.y + 1, position.z), mob, obstacleRow);
-                } else {
-                    // spawn block
-                    GameObject block = ChooseBlock(terrainData);
-                    block.tag = "Block";
-                    GenerateObstacle(new Vector3(newX, position.y + 3, position.z), block, obstacleRow);
-                }
-                
+                currentSizeCluster = Random.Range(MIN_SIZE_CLUSTER, MAX_SIZE_CLUSTER + 1);
+                currentSizeMargin = Random.Range(MIN_SIZE_MARGIN, MAX_SIZE_MARGIN + 1);
             }
         } 
         
         countOfGeneratedRows++;
+    }
+
+    private void spawnRowCluster(float startX, int amount, TerrainData terrainData, Vector3 position, GameObject obstacleRow) {
+        for (int i = 0; i < amount; i++) {
+            float newX = startX + (i * sizeOfBlock);
+            GameObject block = ChooseBlock(terrainData);
+            block.tag = "Block";
+            GenerateObstacle(new Vector3(newX, position.y + 3, position.z), block, obstacleRow);
+        }
+        
+    }
+
+    private void spawnRowMargin(float startX, int amount, TerrainData terrainData, Vector3 position, GameObject obstacleRow) {
+        // Chance of spawning nothing vs. block vs. mob
+        float totalChance = chanceForBlock + chanceForMob + chanceForNothing;
+
+        // spawns an object (or not) based on probabilities for the void space
+        
+        for (int i = 0; i < amount; i++) {
+            float randomNumber = Random.Range(0f, totalChance);
+            float newX = startX + (i * sizeOfBlock);
+            if (randomNumber <= chanceForNothing) {
+                // do nothing
+                return;
+            } else if (randomNumber <= chanceForMob + chanceForNothing) {
+                // spawn mob
+                
+                GameObject mob = ChooseMob(terrainData);
+                mob.tag = "Mob";
+                GenerateObstacle(new Vector3(newX, position.y + 1, position.z), mob, obstacleRow);
+            } else {
+                // spawn block
+                GameObject block = ChooseBlock(terrainData);
+                block.tag = "Block";
+                GenerateObstacle(new Vector3(newX, position.y + 3, position.z), block, obstacleRow);
+            }
+        }
     }
 
     public GameObject ChooseBlock(TerrainData terrainData) {
